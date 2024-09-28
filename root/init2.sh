66ee86669bae01d18bc472142f518c23b4504d04
mq7253180 <mq7253180@126.com>
2024-09-29 01:36:13 +0800

    xxx

--- root/init2.sh
+++ root/init2.sh
@@ -8,6 +8,10 @@ groupadd mysql
 useradd -r -m -d /usr/mysql -g mysql -s /bin/false mysql
 mkdir -p /logs/mysql
 chown -R mysql:mysql /logs/mysql
-rpm -ivh /quincy/mysql/libaio-0.3.109-13.el7.x86_64.rpm
+#rpm -ivh /quincy/mysql/libaio-0.3.109-13.el7.x86_64.rpm
+cd /quincy/mysql/libaio-src
+make install
+ln -sf /usr/lib/libaio.so.1.0.2 /usr/lib64/libaio.so
+ln -sf /usr/lib/libaio.so.1.0.2 /usr/lib64/libaio.so.1
 nohup /quincy/mysql/mysql/bin/mysqld --defaults-file=/quincy/mysql/my-master.cnf --user=mysql 2>&1 &
 nohup java -jar /quincy/github/springcloud/target/quincy-springcloud-0.0.1-SNAPSHOT.jar > spcl.log 2>&1 &
